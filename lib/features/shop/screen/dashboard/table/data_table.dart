158776e870f3d81dc33ceee78d67b6211e8ad919
EIBSDeveloper <testing@eibsglobal.com>
2025-03-01 18:10:35 +0530

    update

---
+++ lib/features/shop/screen/dashboard/table/data_table.dart
@@ -0,0 +1,10 @@
+import '../../../../../utils/constants/path_provider.dart';
+
+class DashboardOrderTable extends StatelessWidget {
+  const DashboardOrderTable({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
